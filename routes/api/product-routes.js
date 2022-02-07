594a0f54074cef3a4b74a8d3812d19e412cb0a9f
steelekreis <steele.kreis@gmail.com>
2022-02-07 00:19:47 -0700

    forgot the delete restful route

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -109,7 +109,22 @@ router.put('/:id', (req, res) => {
 });
 
 router.delete('/:id', (req, res) => {
-  // delete one product by its `id` value
+  Product.destroy({
+    where: {
+      id: req.params.id
+    }
+  })
+    .then(productData => {
+      if (!productData) {
+        res.status(404).json({ message: 'No Product found with that ID.' });
+        return;
+      }
+      res.json(productData);
+    })
+    .catch(err => {
+      console.log(err);
+      res.status(500).json(err);
+    });
 });
 
 module.exports = router;
